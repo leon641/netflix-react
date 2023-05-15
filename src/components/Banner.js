7326009de540c9bc052f1206be0a10aa94f13492
leon641 <leonshoushan54@gmail.com>
2023-05-15 15:41:10 +0300

    setup components+css,list of movies + axios req

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,7 +1,25 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import '../assets/style/Banner.css'
+import axios from '../axios';
+import Requests from '../Requests';
 
 function Banner() {
+    const [movie, setMovie] = useState([]);
+    useEffect(() => {
+        async function fetchData() {
+            const request = await axios.get(Requests.fetchNetflixOriginals)
+            setMovie(
+                request.data.results[
+                    Math.floor(Math.random() * request.data.results.length - 1)
+                ]
+            )
+            return request
+        }
+        fetchData()
+    },[])
+
+    console.log(movie);
+
 
     function truncate(string, n) {
             return string?.length > n ? string.substr(0, n - 1) + '... ' : string
@@ -11,24 +29,21 @@ function Banner() {
     <header className='banner' 
     style={{
         backgroundSize: "cover",
-        backgroundImage: `url("https://upload.wikimedia.org/wikipedia/commons/thumb/c/cd/Black_flag.svg/1200px-Black_flag.svg.png")`,
-        backgroundPosition: "center center"
+        backgroundImage: `url("https://image.tmdb.org/t/p/original/${movie?.backdrop_path}")`,
+        // backgroundPosition: "center center"
     }}
     >
 
     <div className='banner-contents'>
-        <h1 className='banner-title'>Movie Name</h1>
+        <h1 className='banner-title'>
+            {movie?.title || movie?.name || movie?.original_name}
+        </h1>
         <div className='banner-buttons'>
             <button className='banner-button'>Play</button>
             <button className='banner-button'>My List</button>
         </div>
         <h1 className='banner-description'>
-           {truncate(`this is a test desc this is a test desc this is a test desc
-           this is a test desc this is a test desc this is a test desc this is a test desc
-           this is a test desc this is a test desc this is a test descthis is a test desc
-           this is a test descthis is a test descthis is a test descthis is a test desc
-           this is a test descthis is a test descthis is a test descthis is a test desc
-           this is a test descthis is a test descthis is a test descthis is a test desc`, 150)} 
+           {truncate(movie?.overview,150)}
            </h1>
     </div>
     <div className='banner--fadeBottom' />
--- src/components/HomeScreen.js
+++ src/components/HomeScreen.js
@@ -2,6 +2,8 @@ import React from 'react'
 import Nav from '../components/Nav.js'
 import Banner from '../components/Banner.js'
 import '../assets/style/HomeScreen.css'
+import requests from '../Requests.js'
+import Row from '../components/Row.js'
 
 
 function HomeScreen() {
@@ -10,9 +12,21 @@ function HomeScreen() {
         <Nav /> 
 
         <Banner />      
-        {/* BANNER*/}
 
         {/* ROW*/}
+        <Row 
+        title='NETFLIX ORIGINALS' 
+        fetchUrl={requests.fetchNetflixOriginals} 
+        isLargeRow
+        /> 
+        <Row title='Trending Now' fetchUrl={requests.fetchTrending} /> 
+        <Row title='Top Rated' fetchUrl={requests.fetchTopRated} /> 
+        <Row title='Action Movies' fetchUrl={requests.fetchActionMovies} /> 
+        <Row title='Comedy Movies' fetchUrl={requests.fetchComedyMovies} /> 
+        <Row title='Horror Movies' fetchUrl={requests.fetchHorrorMovies} /> 
+        <Row title='Romance Movies' fetchUrl={requests.fetchRomanceMovies} /> 
+        <Row title='Documentaries' fetchUrl={requests.fetchDocumentaries} /> 
+       
     </div>
   )
 }
